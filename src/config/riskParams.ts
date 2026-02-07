73b1ac81596e96d0d1756ff3e14d075d534986ac
EC2 Default User <ec2-user@ip-172-31-1-67.eu-west-1.compute.internal>
2026-02-07 20:35:14 +0000

    logs

--- src/config/riskParams.ts
+++ src/config/riskParams.ts
@@ -9,9 +9,9 @@
 
 export const RISK_PARAMS = {
   /** Maximum total notional across both venues */
-  maxNotional: 490.0,
+  maxNotional: 500.0,
   /** Maximum notional per venue */
-  maxNotionalPerVenue: 245.0,
+  maxNotionalPerVenue: 250.0,
   /** Number of contracts per trade leg */
   qtyPerTrade: 1,
   /** Minimum net edge after fees/slippage required to trade ($0.04) */
